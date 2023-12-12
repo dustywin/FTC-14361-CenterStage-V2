e1ceb4991c33ec414049f3a6c4b17447c4129d4a
gulin225 <ggulin225@gmail.com>
2023-12-12 15:17:07 -0600

    centerTape and leftTape LB

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/LeftBlue/CenterTapeLB.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/LeftBlue/CenterTapeLB.java
@@ -16,11 +16,43 @@ public class CenterTapeLB {
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(15, 61, Math.toRadians(90)))
                                 //pushing onto center tape
+
                                 .lineToConstantHeading(new Vector2d(15, 31))
+                                .addDisplacementMarker(0, () -> {
+//                                    bot.setVirtualFourBarPosition(virtualFourBarState.intaking,virtualFourBarExtensionState.extending);
+//                                    bot.setVirtualFourBarState(virtualFourBarState.intaking);
+//
+//                                    bot.setClawPosition(clawState.close);
+//                                    bot.setClawState(clawState.close);
+                                })
+                                .waitSeconds(.25)
+
+                                .addDisplacementMarker(15, () -> {
+//                                    bot.setVirtualFourBarPosition(virtualFourBarState.init, virtualFourBarExtensionState.extending);
+//                                    bot.setVirtualFourBarState(virtualFourBarState.init);
+//
+//                                    bot.setWristPosition(wristState.sideways);
+//                                    bot.setWristState(wristState.sideways);
+                                })
+                                .waitSeconds(.5)
+
                                 //going to the middle of all of the tape
                                 .lineToConstantHeading(new Vector2d(15, 33))
                                 //going to backboard
+                                .addDisplacementMarker(40, () -> {
+//                                 bot.setVirtualFourBarPosition(virtualFourBarState.outtaking, virtualFourBarExtensionState.extending);
+//                                  bot.setVirtualFourBarState(virtualFourBarState.outtaking);
+//
+//                                   bot.setOuttakeSlidePosition(outtakeSlidesState.LOWOUT, extensionState.extending);
+                                })
+                                .waitSeconds(.5)
+                                .addDisplacementMarker(65, () -> {
+//                                   bot.setClawState(clawState.open);
+//                                   bot.setClawPosition(clawState.open);
+                                })
+
                                 .lineToLinearHeading(new Pose2d(49, 36, Math.toRadians(180)))
+                                .waitSeconds(1)
                                 //going to park
                                 .lineToConstantHeading(new Vector2d(48, 61))
                                 //finish park
--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/LeftBlue/LeftTapeLB.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/LeftBlue/LeftTapeLB.java
@@ -16,8 +16,72 @@ public class LeftTapeLB {
 
                 .followTrajectorySequence(drive ->
                                 drive.trajectorySequenceBuilder(new Pose2d(15, 61, Math.toRadians(90)))
+                                        .lineToConstantHeading(new Vector2d(25, 25.5))
+
+
+                                        .addDisplacementMarker(0, () -> {
+//
+//                                            bot.setVirtualFourBarPosition(virtualFourBarState.intaking,virtualFourBarExtensionState.extending);
+//                                            bot.setVirtualFourBarState(virtualFourBarState.intaking);
+//
+//                                            bot.setClawPosition(clawState.close);
+//                                            bot.setClawState(clawState.close);
+                                        })
+
+                                        .addDisplacementMarker(15, () -> {
+//                                            bot.setVirtualFourBarPosition(virtualFourBarState.init, virtualFourBarExtensionState.extending);
+//                                            bot.setVirtualFourBarState(virtualFourBarState.init);
+//
+//
+//                                            bot.setWristPosition(wristState.sideways);
+//                                            bot.setWristState(wristState.sideways);
+                                        })
+
+                                        .lineToConstantHeading(new Vector2d(30, 25.5))
+                                        .addDisplacementMarker(60, () -> {
+//                                 bot.setVirtualFourBarPosition(virtualFourBarState.outtaking, virtualFourBarExtensionState.extending);
+//                                  bot.setVirtualFourBarState(virtualFourBarState.outtaking);
+//
+//                                   bot.setOuttakeSlidePosition(outtakeSlidesState.LOWOUT, extensionState.extending);
+                                        })
+
+                                        .lineToConstantHeading(new Vector2d(25, 25.5))
+
+                                        .lineToConstantHeading(new Vector2d(25, 30.5))
+
+                                        .lineToConstantHeading(new Vector2d(35, 30.5))
+
+                                        .lineToConstantHeading(new Vector2d(62, 30.5))
+                                        .lineToConstantHeading(new Vector2d(61, 30.5))
+
+
+                                        .waitSeconds(.25)
+
+
+                                        .addDisplacementMarker(90, () -> {
+
+//
+//                                            bot.setClawState(clawState.open);
+//                                            bot.setClawPosition(clawState.open);
+//
+
+                                        })
+                                        .waitSeconds(1)
+
+
+                                        .lineToConstantHeading(new Vector2d(55, 52))
+
+                                        .lineToConstantHeading(new Vector2d(68, 52))
+
+                                        .addDisplacementMarker(105,() -> {
+//                                            bot.setOuttakeSlidePosition(outtakeSlidesState.STATION, extensionState.extending);
+//                                            bot.setVirtualFourBarPosition(virtualFourBarState.init, virtualFourBarExtensionState.extending);
+                                        })
+                                        .waitSeconds(1)
 
                                         .build()
+
+
 
                 );
 
