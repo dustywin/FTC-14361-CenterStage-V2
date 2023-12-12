d8a408240ccb3e50085d9d9b2ab47c276fb3759b
AravNeroth <reacharav@outlook.com>
2023-12-12 17:04:44 -0600

    Left Right tape completed- ALL. RED TAPE PATHS DONE!

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/LeftRed/LeftTapeLR.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/LeftRed/LeftTapeLR.java
@@ -13,6 +13,26 @@ public class LeftTapeLR {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10.5)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-35, -60, Math.toRadians(270)))
+                                .lineToConstantHeading(new Vector2d(-47, -43))
+                                .waitSeconds(.5)
+                                .lineToConstantHeading(new Vector2d(-47, -50))
+                                .lineToConstantHeading(new Vector2d(-34, -50))
+                                .waitSeconds(.5)
+                                .lineToLinearHeading(new Pose2d(-34, -12, Math.toRadians(180)))
+                                .waitSeconds(.5)
+                                .lineToConstantHeading(new Vector2d(37, -12))
+                                .waitSeconds(.5)
+                                .lineToConstantHeading(new Vector2d(37, -29))
+                                .waitSeconds(.5)
+                                .lineToConstantHeading(new Vector2d(49.5, -29))
+                                .waitSeconds(.5)
+                                .lineToConstantHeading(new Vector2d(37, -29))
+                                .lineToConstantHeading(new Vector2d(37, -12))
+                                .waitSeconds(.5)
+                                .lineToConstantHeading(new Vector2d(57, -12))
+
+
+                                .waitSeconds(.5)
 
                                 .build()
 
