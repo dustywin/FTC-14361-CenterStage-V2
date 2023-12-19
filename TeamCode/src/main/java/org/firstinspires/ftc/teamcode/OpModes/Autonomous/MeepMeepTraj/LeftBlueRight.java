ab50fb10586d2e044674e6529ba9182476e5cda5
AravNeroth <reacharav@outlook.com>
2023-12-19 10:13:56 -0600

    LeftBlue Path templates done
    ready to import the TrajSeqBuilder from MeepMeep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/MeepMeepTraj/LeftBlueRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/MeepMeepTraj/LeftBlueRight.java
@@ -1,4 +1,43 @@
 package org.firstinspires.ftc.teamcode.OpModes.Autonomous.MeepMeepTraj;
 
-public class LeftBlueRight {
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.apache.commons.math3.geometry.euclidean.twod.Line;
+import org.firstinspires.ftc.teamcode.OpModes.Autonomous.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.OpModes.Autonomous.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.Subsystems.Robot;
+
+@Autonomous(name = "LeftBlueRight")
+
+public class LeftBlueRight extends LinearOpMode {
+    Robot bot;
+    public TrajectorySequence fullPath;
+    @Override
+    public void runOpMode() {
+
+        bot = new Robot(hardwareMap, telemetry);
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Pose2d newStart = new Pose2d(-35, -60, 270);
+        drive.setPoseEstimate(newStart);
+
+
+        TrajectorySequence startToFinish = drive.trajectorySequenceBuilder(newStart)
+                .waitSeconds(.5)
+
+                .build();
+
+        fullPath = startToFinish;
+
+        waitForStart();
+
+        drive.followTrajectorySequence(startToFinish);
+
+
+    }
+    public TrajectorySequence getLeftBlueRight(){
+        return fullPath;
+    }
+
 }
