83bc42423d8ddc61b46fd7117970e0747111d55e
AravNeroth <reacharav@outlook.com>
2023-12-19 10:21:07 -0600

    all classes under 'MeepMeepTraj' ready for import from MeepMeep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/MeepMeepTraj/RightRedRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/MeepMeepTraj/RightRedRight.java
@@ -5,21 +5,36 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.OpModes.Autonomous.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.OpModes.Autonomous.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.Subsystems.Robot;
 
 @Autonomous(name = "RightRedRight")
 
-public class RightRedRight extends LinearOpMode {
-    Robot bot;
-
+public class RightRedRight extends LinearOpMode {Robot bot;
+    public TrajectorySequence fullPath;
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
+
         bot = new Robot(hardwareMap, telemetry);
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         Pose2d newStart = new Pose2d(-35, -60, 270);
         drive.setPoseEstimate(newStart);
 
+
+        TrajectorySequence startToFinish = drive.trajectorySequenceBuilder(newStart)
+                .waitSeconds(.5)
+
+                .build();
+
+        fullPath = startToFinish;
+
         waitForStart();
 
+        drive.followTrajectorySequence(startToFinish);
+
+
+    }
+    public TrajectorySequence getRightRedRight(){
+        return fullPath;
     }
 }
