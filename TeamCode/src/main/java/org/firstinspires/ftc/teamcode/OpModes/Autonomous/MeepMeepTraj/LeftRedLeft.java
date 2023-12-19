63ebaffe915dbb02900b616ea868ba349b742077
AravNeroth <reacharav@outlook.com>
2023-12-19 16:42:46 -0600

    new passthrough

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/MeepMeepTraj/LeftRedLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/MeepMeepTraj/LeftRedLeft.java
@@ -63,7 +63,36 @@ public class LeftRedLeft extends LinearOpMode {
 
     // i have no idea if this will work, if it does though we're chilling
     public TrajectorySequence getLeftRedLeftTraj(){
-        return fullPath;
+
+        bot = new Robot(hardwareMap, telemetry);
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Pose2d newStart = new Pose2d(-35, -60, 270);
+        drive.setPoseEstimate(newStart);
+
+        TrajectorySequence startToFinish = drive.trajectorySequenceBuilder(newStart)
+                .lineToConstantHeading(new Vector2d(-47, -43))
+                .waitSeconds(.5)
+                .lineToConstantHeading(new Vector2d(-47, -50))
+                .lineToConstantHeading(new Vector2d(-34, -50))
+                .waitSeconds(.5)
+                .lineToLinearHeading(new Pose2d(-34, -12, Math.toRadians(180)))
+                .waitSeconds(.5)
+                .lineToConstantHeading(new Vector2d(37, -12))
+                .waitSeconds(.5)
+                .lineToConstantHeading(new Vector2d(37, -29))
+                .waitSeconds(.5)
+                .lineToConstantHeading(new Vector2d(49.5, -29))
+                .waitSeconds(.5)
+                .lineToConstantHeading(new Vector2d(37, -29))
+                .lineToConstantHeading(new Vector2d(37, -12))
+                .waitSeconds(.5)
+                .lineToConstantHeading(new Vector2d(57, -12))
+
+                .waitSeconds(.5)
+
+                .build();
+
+        return startToFinish;
     }
 
 }
